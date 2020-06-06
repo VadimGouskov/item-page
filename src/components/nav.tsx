2cc7b447ce091b13b78f0a91f497cff1d9f79cbd
VadimGouskov <vadimgouskov@hotmail.com>
2020-06-06 15:20:54 +0200

    added favicaon changed title

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,7 +5,10 @@ const Nav: React.FC = () => {
         <nav className="bp3-navbar bp3-dark">
         <div> 
             <div className="bp3-navbar-group bp3-align-left">
-                <div className="bp3-navbar-heading">Rick and Morty Lookup</div>
+                <div className="bp3-navbar-heading"> 
+{/*                     <img className="header-logo" src='logo-512x512.png'></img>
+ */}                    <h1>Book of Rick</h1>
+                </div>
             </div>
             <div className="bp3-navbar-group bp3-align-right">
                 {/* <button className="bp3-button bp3-minimal">Home</button>
