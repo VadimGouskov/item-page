482a0c0547dafae77287a769ed2fa1c19b152308
VadimGouskov <vadimgouskov@hotmail.com>
2021-12-21 20:44:15 +0100

    added default port

--- src/components/FeedStatus.tsx
+++ src/components/FeedStatus.tsx
@@ -1,38 +1,31 @@
-import React from 'react';
-import { bindActionCreators } from 'redux';
-import State from '../types/state';
-import { Spinner, Intent } from '@blueprintjs/core';
-import { connect } from 'react-redux';
+import React from "react";
+import { bindActionCreators } from "redux";
+import State from "../types/state";
+import { Spinner, Intent } from "@blueprintjs/core";
+import { connect } from "react-redux";
 
-type FeedStatusProps =
-ReturnType<typeof mapStateToProps> &
-ReturnType<typeof mapDispatchToProps>
+type FeedStatusProps = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
 
 const FeedStatus: React.FC<FeedStatusProps> = (props) => {
-    return (
-        <div className="columns">
-            <div className="column is-2 has-padding-left-40">
-                {/* total: */}
-            </div>
-            <div className="column">
-                {props.pending && 
-                    <Spinner
-                        intent={Intent.PRIMARY}
-                    />
-                }
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="columns">
+      <div className="column is-2 has-padding-left-40">{/* total: */}</div>
+      <div className="column">
+        {props.pending && <Spinner intent={Intent.PRIMARY} />}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state: State) => {
-    return {
-        pending: state.items.pending
-    };
-}
+  return {
+    pending: state.items.pending,
+  };
+};
 
 const mapDispatchToProps = (dispatch: any) => {
-    return bindActionCreators({}, dispatch);
-}
+  return bindActionCreators({}, dispatch);
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(FeedStatus);
--- src/components/ItemFeed.tsx
+++ src/components/ItemFeed.tsx
@@ -1,74 +1,77 @@
-import React, { useEffect } from 'react';
-import Item from './Item';
-import { connect } from 'react-redux';
-import { addItem, loadItems } from '../ducks/items';
-import { bindActionCreators } from 'redux';
-import ItemType from '../types/item';
-import State from '../types/state';
-import Filters, { Genders } from '../types/filters';
+import React, { useEffect } from "react";
+import Item from "./Item";
+import { connect } from "react-redux";
+import { addItem, loadItems } from "../ducks/items";
+import { bindActionCreators } from "redux";
+import ItemType from "../types/item";
+import State from "../types/state";
+import Filters, { Genders } from "../types/filters";
 
 // COMPONENT
-type ItemFeedProps = 
-  ReturnType<typeof mapStateToProps> &
+type ItemFeedProps = ReturnType<typeof mapStateToProps> &
   ReturnType<typeof mapDispatchToProps>;
 
 const ItemFeed: React.FC<ItemFeedProps> = (props) => {
-
   useEffect(() => {
     props.loadItems();
-  }, [props.loadItems])
+  }, [props.loadItems]);
 
   return (
-    <div className='item-container'>
-        <div className='columns is-multiline'>
-            { props.filteredItems.map((item: ItemType) => (
-                <div key={item.id} className='column is-4'>
-                    <Item  item={item}/>
-                </div> 
-            )) }
-        </div>
+    <div className="item-container">
+      <div className="columns is-multiline">
+        {props.filteredItems.map((item: ItemType) => (
+          <div key={item.id} className="column is-4" >
+            <Item item={item} />
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-// METHODS 
+// METHODS
 const getFilteredItems = (items: ItemType[], filters: Filters) => {
-  return items.filter((item: ItemType) => { 
-
+  return items.filter((item: ItemType) => {
     // if no filters are selected, display all items
-    if(!Object.values(filters.gender).some(filter => filter === true)){
-        return items;
+    if (!Object.values(filters.gender).some((filter) => filter === true)) {
+      return items;
     }
 
     // TODO fix this unscalable filtering code
-    if(item.gender === Genders.Male && !filters.gender.male) {
-        return false;
-    } else if(item.gender === Genders.Female && !filters.gender.female) {
-        return false;
-    } else if(item.gender === Genders.Genderless && !filters.gender.genderless) {
-        return false;
-    } else if(item.gender === Genders.Unknown && !filters.gender.unknown) {
-        return false;
+    if (item.gender === Genders.Male && !filters.gender.male) {
+      return false;
+    } else if (item.gender === Genders.Female && !filters.gender.female) {
+      return false;
+    } else if (
+      item.gender === Genders.Genderless &&
+      !filters.gender.genderless
+    ) {
+      return false;
+    } else if (item.gender === Genders.Unknown && !filters.gender.unknown) {
+      return false;
     } else {
-        return true
+      return true;
     }
-  })
-}
+  });
+};
 
 // PROP MAPPING
 const mapStateToProps = (state: State) => {
   return {
     items: state.items,
     filteredItems: getFilteredItems(state.items.items, state.filters),
-    filters: state.filters
-  }
-}
+    filters: state.filters,
+  };
+};
 
 const mapDispatchToProps = (dispatch: any) => {
-    return bindActionCreators({ 
-        addItem,
-        loadItems, 
-    }, dispatch);
-}
+  return bindActionCreators(
+    {
+      addItem,
+      loadItems,
+    },
+    dispatch
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ItemFeed);
